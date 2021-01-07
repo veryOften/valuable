1cc914a96c271e6dad22e562548834663bdfe798
Santos <wjdrms1919@gmail.com>
2021-01-07 23:43:23 +0900

    add second case of error boundary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
-function ErrorFallback({ error }) {
+
+function ErrorFallback1({ error }) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
@@ -10,12 +11,57 @@ function ErrorFallback({ error }) {
   );
 }
 
-function Main({ subject }) {
+function ErrorFallback2({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+function Bomb({ username }) {
+  if (username === "bomb") {
+    throw new Error("💥 CABOOM 💥");
+  }
   return (
     <div>
-      Main {subject.toUpperCase()}
-      <Link to="/farewell">farewell</Link>
+      {`Hi ${username}`}
+      <br />
       <Link to="/greeting">greeting</Link>
+      <br />
+      <Link to="/farewell">farewell</Link>
+    </div>
+  );
+}
+
+function Main() {
+  const [username, setUsername] = React.useState("");
+  const usernameRef = React.useRef(null);
+  return (
+    <div>
+      <label>
+        {`Username (don't type "bomb"): `}
+        <input
+          placeholder={`type "bomb"`}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          ref={usernameRef}
+        />
+      </label>
+      <div>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback2}
+          onReset={() => {
+            setUsername("");
+            usernameRef.current.focus();
+          }}
+          resetKeys={[username]}
+        >
+          <Bomb username={username} />
+        </ErrorBoundary>
+      </div>
     </div>
   );
 }
@@ -24,7 +70,9 @@ function Greeting({ subject }) {
   return (
     <div>
       Hello {subject.toUpperCase()}
+      <br />
       <Link to="/">main</Link>
+      <br />
       <Link to="/farewell">farewell</Link>
     </div>
   );
@@ -33,7 +81,9 @@ function Farewell({ subject }) {
   return (
     <div>
       Goodbye {"s".toUpperCase()}
+      <br />
       <Link to="/">main</Link>
+      <br />
       <Link to="/greeting">greeting</Link>
     </div>
   );
@@ -43,7 +93,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <ErrorBoundary FallbackComponent={ErrorFallback1}>
           <Route exact path="/" component={Main} />
           <Route exact path="/greeting" component={Greeting} />
           <Route exact path="/farewell" component={Farewell} />
